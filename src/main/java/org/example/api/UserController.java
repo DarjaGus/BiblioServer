fb8258162c7bc81c44f7e8031d83802878a29b32
DarjaGus <dar4wei@yandex.ru>
2025-06-05 17:04:20 +0300

    *

--- src/main/java/org/example/api/UserController.java
+++ src/main/java/org/example/api/UserController.java
@@ -3,6 +3,7 @@ package org.example.api;
 import org.example.model.User;
 import org.example.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -11,6 +12,9 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     @PostMapping("/register")
     public User registerUser(@RequestBody User user) {
         return userService.saveUser(user);
@@ -18,6 +22,10 @@ public class UserController {
 
     @PostMapping("/login")
     public User loginUser(@RequestBody User user) {
-        return userService.findByUsername(user.getUsername());
+        User existingUser = userService.findByUsername(user.getUsername());
+        if (existingUser != null && passwordEncoder.matches(user.getPassword(), existingUser.getPassword())) {
+            return existingUser;
+        }
+        return null;
     }
 }
